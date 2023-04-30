83e213bdbdd347da1153744a988cf8628a9a9d7d
Ben Brown <benjaminvsbrown@gmail.com>
2023-04-29 18:41:53 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+import Map from "./components/Map";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<Map />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,204 @@
+import StaticMap from "react-map-gl";
+import DeckGL from "@deck.gl/react";
+import { IconLayer } from "@deck.gl/layers";
+
+import darkMatter from "../mapboxStyles/darkMatter.json";
+import carto_dark_matter_nolabels from "../mapboxStyles/carto_dark_matter_nolabels.json";
+
+import yeezy_pin from "../assets/icons/yeezyLocationPin.svg";
+
+const INITIAL_VIEW_STATE = {
+	longitude: -100,
+	latitude: 40,
+	zoom: 1,
+	maxZoom: 20,
+	pitch: 0,
+	bearing: 0,
+};
+
+// const INITIAL_VIEW_STATE = {
+// 	longitude: -122.466233,
+// 	latitude: 37.684638,
+// 	zoom: 18,
+// 	maxZoom: 15,
+// 	pitch: 0,
+// 	bearing: 0,
+// };
+
+const ICON_MAPPING = {
+	marker: { x: 0, y: 0, width: 128, height: 128, mask: true },
+};
+
+const LOCATIONS = [
+	{
+		name: "Los Angeles",
+		address: `55°31'24.0"N 9°11'50.0"E`,
+		exits: 4214,
+		coordinates: [-118.2437, 34.0522],
+	},
+	{
+		name: "UK London",
+		address: `55°31'24.0"N 9°11'50.0"E`,
+		exits: 4214,
+		coordinates: [-0.116773, 51.510357],
+	},
+];
+
+const coordinatesList = [
+	[-118.2437, 34.0522], // Los Angeles, California, USA
+	[-73.9352, 40.7306], // New York City, New York, USA
+	[-79.3832, 43.6532], // Toronto, Ontario, Canada
+	[-97.7431, 30.2672], // Austin, Texas, USA
+	[-87.6298, 41.8781], // Chicago, Illinois, USA
+	[-123.1207, 49.2827], // Vancouver, British Columbia, Canada
+	[-75.1652, 39.9526], // Philadelphia, Pennsylvania, USA
+	[-89.4012, 43.0731], // Madison, Wisconsin, USA
+	[-104.9903, 39.7392], // Denver, Colorado, USA
+	[-116.5453, 33.8303], // Palm Springs, California, USA
+];
+
+const locationData = [
+	{
+		id: 1,
+		name: "Location 1",
+		longitude: -118.2437,
+		latitude: 34.0522,
+	},
+	{
+		id: 2,
+		name: "Location 2",
+		longitude: -73.9352,
+		latitude: 40.7306,
+	},
+	{
+		id: 3,
+		name: "Location 3",
+		longitude: -79.3832,
+		latitude: 43.6532,
+	},
+	{
+		id: 4,
+		name: "Location 4",
+		longitude: -97.7431,
+		latitude: 30.2672,
+	},
+	{
+		id: 5,
+		name: "Location 5",
+		longitude: -87.6298,
+		latitude: 41.8781,
+	},
+	{
+		id: 6,
+		name: "Location 6",
+		longitude: -123.1207,
+		latitude: 49.2827,
+	},
+	{
+		id: 7,
+		name: "Location 7",
+		longitude: -75.1652,
+		latitude: 39.9526,
+	},
+	{
+		id: 8,
+		name: "Location 8",
+		longitude: -89.4012,
+		latitude: 43.0731,
+	},
+	{
+		id: 9,
+		name: "Location 9",
+		longitude: -104.9903,
+		latitude: 39.7392,
+	},
+	{
+		id: 10,
+		name: "Location 10",
+		longitude: -116.5453,
+		latitude: 33.8303,
+	},
+];
+
+// const locationData = coordinatesList.map((coordinates, index) => ({
+// 	id: index + 1,
+// 	name: `Location ${index + 1}`,
+// 	longitude: coordinates[0],
+// 	latitude: coordinates[1],
+// }));
+
+console.log({ locationData });
+
+const yeezyIconLayer = new IconLayer({
+	id: "yeezy-icon-layer",
+	data: LOCATIONS, // Your location data
+	pickable: true,
+	iconAtlas: yeezy_pin,
+	iconMapping: {
+		yeezyLocationPin: {
+			x: 0,
+			y: 0,
+			width: 32,
+			height: 32,
+			mask: true,
+		},
+	},
+	getPosition: (d) => [d.longitude, d.latitude],
+	getIcon: (d) => "yeezyLocationPin",
+	getSize: (d) => 5,
+	getColor: (d) => [255, 140, 0],
+	getIconOffsets: (d) => [0, -128],
+});
+
+const sampleIcon = new IconLayer({
+	id: "icon-layer",
+	data: LOCATIONS,
+	pickable: true,
+	// iconAtlas and iconMapping are required
+	// getIcon: return a string
+	iconAtlas:
+		"https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png",
+	iconMapping: ICON_MAPPING,
+	getIcon: (d) => "marker",
+	sizeScale: 8,
+	getPosition: (d) => d.coordinates,
+	getSize: (d) => 5,
+	getColor: (d) => [231, 226, 191],
+	getPixelOffset: (d) => [0, -32],
+});
+
+{
+	/* <div id="tooltip" style="position: absolute; z-index: 100; background-color: rgba(0, 0, 0, 0.8); color: white; padding: 8px; border-radius: 4px; pointer-events: none; display: none;"></div> */
+}
+
+export default function Map() {
+	const layers = [sampleIcon];
+	return (
+		<DeckGL
+			layers={layers}
+			initialViewState={INITIAL_VIEW_STATE}
+			controller={true}
+			getTooltip={({ object }) =>
+				object && {
+					html: `<div>${object.name}</div><div>${object.address}</div>`,
+					style: {
+						color: "#e7e2bf",
+						backgroundColor: "rgba(0, 0, 0)",
+						fontSize: "14.667px",
+						// paddingBottom: "20px",
+						// paddingTop: "20px",
+						padding: "20px 50px",
+						borderRadius: "8px",
+					},
+				}
+			}
+		>
+			<StaticMap
+				// mapStyle="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
+				// mapStyle="https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json"
+				mapStyle={carto_dark_matter_nolabels}
+				mapboxAccessToken={import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}
+			/>
+		</DeckGL>
+	);
+}
