068dc20f5fe8c5d03b4024831f84b44f73d4155e
Ben Brown <benjaminvsbrown@gmail.com>
2023-04-29 19:55:34 -0700

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import "./fonts/FoundationSansBlackCdIt.otf";
+import { Analytics } from "@vercel/analytics/react";
+
 import Map from "./components/Map";
 
 function App() {
 	return (
 		<>
 			<Map />
+			<Analytics />
 		</>
 	);
 }
